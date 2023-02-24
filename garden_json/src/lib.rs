be47c9cea10b6a0769a1ccd8185924b9adcb8765
Jonathan Orman <jonathanpcorman@gmail.com>
2023-02-24 16:35:17 +0000

    Add tests for JsonToStringConverter

--- garden_json/src/lib.rs
+++ garden_json/src/lib.rs
@@ -37,22 +37,22 @@ mod tests {
     use rstest::rstest;
     use serde_json::json;
 
-    use crate::{ConvertJsonToValue, JsonToF32Converter};
+    use crate::{ConvertJsonToValue, JsonToF32Converter, JsonToStringConverter};
 
     #[rstest]
     #[case(1.23)]
     #[case(45.67)]
     #[case(0.0)]
     fn when_a_json_to_f32_converter_converts_an_f32_json_value_to_f32_then_it_is_converted_to_f32(
-        #[case] val: f32,
+        #[case] value: f32,
     ) {
-        let f32_value = json!(val);
+        let f32_value = json!(value);
 
         let json_to_f32_converter = JsonToF32Converter::new();
 
         let result = json_to_f32_converter.convert_json_to_value(&f32_value);
 
-        assert_eq!(val, result);
+        assert_eq!(value, result);
     }
 
     #[rstest]
@@ -62,15 +62,47 @@ mod tests {
     #[case(-127, -127.0)]
     #[case(0, 0.0)]
     fn when_a_json_to_f32_converter_converts_an_i8_json_value_to_f32_then_it_is_converted_to_f32(
-        #[case] val: i8,
+        #[case] value: i8,
         #[case] expected_result: f32,
     ) {
-        let f32_value = json!(val);
+        let f32_value = json!(value);
 
         let json_to_f32_converter = JsonToF32Converter::new();
 
         let result = json_to_f32_converter.convert_json_to_value(&f32_value);
 
         assert_eq!(expected_result, result);
+    }
+
+    #[rstest]
+    #[case("val")]
+    #[case(" ")]
+    #[case("")]
+    fn when_a_json_to_string_converter_converts_a_string_json_value_to_string_then_it_is_converted_to_string(
+        #[case] value: String,
+    ) {
+        let string_value = json!(value);
+
+        let json_to_string_converter = JsonToStringConverter::new();
+
+        let result = json_to_string_converter.convert_json_to_value(&string_value);
+
+        assert_eq!(value, result);
+    }
+
+    #[rstest]
+    #[case("val")]
+    #[case(" ")]
+    #[case("")]
+    fn when_a_json_to_string_converter_converts_a_str_json_value_to_string_then_it_is_converted_to_string(
+        #[case] value: &str,
+    ) {
+        let string_value = json!(value);
+
+        let json_to_string_converter = JsonToStringConverter::new();
+
+        let result = json_to_string_converter.convert_json_to_value(&string_value);
+
+        assert_eq!(value, result);
     }
 }
