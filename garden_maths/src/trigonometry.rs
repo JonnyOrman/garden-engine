1cef672ba8db9a9599f50d5b4cf42b627191921e
Jonathan Orman <jonathanpcorman@gmail.com>
2023-07-24 12:45:43 +0100

    Add maths dependency on content

--- garden_maths/src/trigonometry.rs
+++ garden_maths/src/trigonometry.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 pub trait ConvertDegreesToRadians {
     fn convert_degrees_to_radians(&self, degrees: f64) -> f64;
 }
@@ -35,7 +37,7 @@ impl CalculateAdjacent for AdjacentCalculator {
 }
 
 pub trait CalculateOpposite {
-    fn calculate_adjacent(&self, hypotenuse: f64, angle_radians: f64) -> f64;
+    fn calculate_opposite(&self, hypotenuse: f64, angle_radians: f64) -> f64;
 }
 
 pub struct OppositeCalculator {}
@@ -50,4 +52,77 @@ impl CalculateOpposite for OppositeCalculator {
     fn calculate_opposite(&self, hypotenuse: f64, angle_radians: f64) -> f64 {
         angle_radians.sin() * hypotenuse
     }
+}
+
+pub trait CalculateTrigonometry:
+    ConvertDegreesToRadians + CalculateAdjacent + CalculateOpposite
+{
+}
+
+pub struct TrigonometryCalculator<
+    TDegreesToRadiansConverter,
+    TAdjacentCalculator,
+    TOppositeCalculator,
+> {
+    degrees_to_radians_converter: Rc<TDegreesToRadiansConverter>,
+    adjacent_calculator: Rc<TAdjacentCalculator>,
+    opposite_calculator: Rc<TOppositeCalculator>,
+}
+
+impl<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator>
+    TrigonometryCalculator<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator>
+{
+    fn new(
+        degrees_to_radians_converter: Rc<TDegreesToRadiansConverter>,
+        adjacent_calculator: Rc<TAdjacentCalculator>,
+        opposite_calculator: Rc<TOppositeCalculator>,
+    ) -> Self {
+        Self {
+            degrees_to_radians_converter,
+            adjacent_calculator,
+            opposite_calculator,
+        }
+    }
+}
+
+impl<
+        TDegreesToRadiansConverter: ConvertDegreesToRadians,
+        TAdjacentCalculator,
+        TOppositeCalculator,
+    > ConvertDegreesToRadians
+    for TrigonometryCalculator<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator>
+{
+    fn convert_degrees_to_radians(&self, degrees: f64) -> f64 {
+        self.degrees_to_radians_converter
+            .convert_degrees_to_radians(degrees)
+    }
+}
+
+impl<TDegreesToRadiansConverter, TAdjacentCalculator: CalculateAdjacent, TOppositeCalculator>
+    CalculateAdjacent
+    for TrigonometryCalculator<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator>
+{
+    fn calculate_adjacent(&self, hypotenuse: f64, angle_radians: f64) -> f64 {
+        self.adjacent_calculator
+            .calculate_adjacent(hypotenuse, angle_radians)
+    }
+}
+
+impl<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator: CalculateOpposite>
+    CalculateOpposite
+    for TrigonometryCalculator<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator>
+{
+    fn calculate_opposite(&self, hypotenuse: f64, angle_radians: f64) -> f64 {
+        self.opposite_calculator
+            .calculate_opposite(hypotenuse, angle_radians)
+    }
+}
+
+impl<
+        TDegreesToRadiansConverter: ConvertDegreesToRadians,
+        TAdjacentCalculator: CalculateAdjacent,
+        TOppositeCalculator: CalculateOpposite,
+    > CalculateTrigonometry
+    for TrigonometryCalculator<TDegreesToRadiansConverter, TAdjacentCalculator, TOppositeCalculator>
+{
 }
